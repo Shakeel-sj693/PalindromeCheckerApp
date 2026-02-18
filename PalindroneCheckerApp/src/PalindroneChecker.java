d412e5981bd795bcfe5335e028b7956598dc29fd
Shakeel-sj693 <ss1279@srmist.edu.in>
2026-02-18 09:31:29 +0530

    shakeel  Added Shakeel-sj693

---
+++ PalindroneCheckerApp/src/PalindroneChecker.java
@@ -0,0 +1,5 @@
+public class PalindroneChecker {
+    public static void main(String[] args){
+        System.out.println("Welcome to pallindrome checker app management system");
+        System.out.println("Modify the logic ");
+    }}
