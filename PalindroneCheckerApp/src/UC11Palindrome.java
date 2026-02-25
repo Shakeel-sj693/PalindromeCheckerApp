687bd92f058569cb643821b88e521be4d75ba618
SHAKEEL SJ <ss1279@srmist.edu.in>
2026-02-25 09:37:54 +0530

    Add UC11Palindrome class

---
+++ PalindroneCheckerApp/src/UC11Palindrome.java
@@ -0,0 +1,19 @@
+public class UC11Palindrome {
+
+    public boolean checkPalindrome(String input) {
+        if (input == null) return false;
+
+        String cleaned = input.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = cleaned.length() - 1;
+
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) return false;
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
