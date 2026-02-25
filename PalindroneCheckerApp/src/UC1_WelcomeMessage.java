e431e24bcd7087cad2255c883eada2b5e87fe038
SHAKEEL SJ <ss1279@srmist.edu.in>
2026-02-25 10:00:44 +0530

    Add UC1 welcome message entry point

---
+++ PalindroneCheckerApp/src/UC1_WelcomeMessage.java
@@ -0,0 +1,10 @@
+public class UC1_WelcomeMessage {
+
+    private static final String APP_NAME = "Palindrome Checker Application";
+    private static final String APP_VERSION = "1.0.0";
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to " + APP_NAME);
+        System.out.println("Version: " + APP_VERSION);
+    }
+}
